e0b7ef1368de3124ebb31fd60a0cc1f3a4c3583c
Sharad <sharadgoyal1998@gmail.com>
2026-04-22 14:31:44 +0530

    SG: fixes

--- chatbot_using_langgraph/chatbot_frontend.py
+++ chatbot_using_langgraph/chatbot_frontend.py
@@ -1,55 +1,42 @@
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage
 import streamlit as st
 from chatbot_backend import chatbot
 
-# If checkpoint is there then we need to pass thread_id to the frontend too!
-CONFIG = {"configurable":{"thread_id":1}}
+CONFIG = {"configurable": {"thread_id": 1}}
 
 user_input = st.chat_input('Type here')
-# st.session_state -> dict -> when pressing enter (except manually refreshing the page)
-
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
-
-# displaying conversation history
+# Display history
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
-        st.text(message['content'])
-
+        st.markdown(message['content'])
 
 if user_input:
 
-    #Adding msg to message history of the user
-    st.session_state['message_history'].append({'role':'user', 'content': user_input})
+    # Store user message
+    st.session_state['message_history'].append({
+        'role': 'user',
+        'content': user_input
+    })
+
     with st.chat_message('user'):
-        st.text(user_input)
+        st.markdown(user_input)
 
+    # Call LLM
+    response = chatbot.invoke(
+        {'messages': [HumanMessage(content=user_input)]},
+        config=CONFIG
+    )['messages'][-1].content
 
-    initial_state = {
-        'messages' : [HumanMessage(content = user_input)]
-    }
-    
-    # Calling LLM 
-   # response = chatbot.invoke(initial_state,config= CONFIG)['messages'][-1].content
-
-    # To make the response in stream mode
-    
-
-
-    #Adding msg to message history of the AI    
-
+    # Display assistant response
     with st.chat_message('assistant'):
-        ai_message = st.write_stream(
-            message_chunk.content for message_chunk, metadata in chatbot.stream(
-                {
-                    'messages' : [HumanMessage(content = user_input ) ]
-                },
-                config = CONFIG,
-                stream_mode = 'messages'
-            )
-        )
+        st.markdown(response)
 
-    st.session_state['message_history'].append({'role':'Assistant', 'content': ai_message})
-    
+    # Store assistant response (FIXED)
+    st.session_state['message_history'].append({
+        'role': 'assistant',
+        'content': response
+    })
